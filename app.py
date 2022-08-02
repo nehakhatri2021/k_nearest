c2dacfce556534e244fd7eda36514110d7f77164
nehakhatri2021 <110082217+nehakhatri2021@users.noreply.github.com>
2022-08-02 10:06:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 pickle_in = open("knn_model.pkl","rb")
 model=pickle.load(pickle_in)
 dataset= pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2:4]].values
+X = dataset.iloc[:, 2:4].values
 def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
   output= model.predict(sc.transform([[Age,EstimatedSalary]]))
   print("Purchased", output)
